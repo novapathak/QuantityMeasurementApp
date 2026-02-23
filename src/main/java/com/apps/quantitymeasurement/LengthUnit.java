021fdfa34a4b0118340c85d453f99b22da400456
novapathak <novapathak@gmail.com>
2026-02-24 01:51:24 +0530

    [Nova]: added lengthUnit file in UC04

--- src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -2,7 +2,8 @@ package com.apps.quantitymeasurement;
 
  enum LengthUnit {
 	 FEET(1.0),
-	 INCH(1.0 / 12.0);
+	 INCH(1.0 / 12.0), YARDS(36.0), CENTIMETERS(0.393701);
+
 	 
 	 private final double conversionFactorToFeet;
 	 
