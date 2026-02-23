262521d5a37f29914bea5f9fd2225ffb6154d935
novapathak <novapathak@gmail.com>
2026-02-24 01:51:52 +0530

    [Nova]: added QuantityMeasurement file in UC04

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -8,25 +8,35 @@ import java.util.*;
 public class QuantityMeasurementApp 
 {
 	
+	public static void demonstrateLengthComparison(double value1, LengthUnit unit1, double value2, LengthUnit unit2) {
+
+		Length l1 = new Length(value1, unit1);
+		Length l2 = new Length(value2, unit2);
+
+		boolean result = l1.equals(l2);
+
+		System.out.println("Length 1: " + l1);
+		System.out.println("Length 2: " + l2);
+		System.out.println("Are Both length equal? " + result);
+	}
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
 		
 		
-		// Cross-unit comparison
-		Length l1 = new Length(1.0, LengthUnit.FEET);
-		Length l2 = new Length(12.0, LengthUnit.INCH);
-		
-		System.out.println("Input: "+ l1+ " and "+ l2 );
-		System.out.println("Output: Equal "+ l1.equals(l2));
-		
-		
-		// Same-unit comparison
-		Length l3 = new Length(1.0, LengthUnit.INCH);
-		Length l4 = new Length(12.0, LengthUnit.INCH);
-		
-		// Results of comparison
-        System.out.println("Input values: " + l3 + " and " + l4);
-        System.out.println("Output: Equal " + l3.equals(l4) );
+		// Feet and Inches
+				demonstrateLengthComparison(1.0, LengthUnit.FEET, 12.0, LengthUnit.INCH);
+
+				// Yards and Inches
+				demonstrateLengthComparison(1.0, LengthUnit.YARDS, 36.0, LengthUnit.INCH);
+
+				// Centimeters and Inches
+				demonstrateLengthComparison(100.0, LengthUnit.CENTIMETERS, 39.3701, LengthUnit.INCH);
+
+				// Feet and Yards
+				demonstrateLengthComparison(3.0, LengthUnit.FEET, 1.0, LengthUnit.YARDS);
+
+				// Centimeters and Feet
+				demonstrateLengthComparison(30.48, LengthUnit.CENTIMETERS, 1.0, LengthUnit.FEET);
 		
     sc.close();	
       
