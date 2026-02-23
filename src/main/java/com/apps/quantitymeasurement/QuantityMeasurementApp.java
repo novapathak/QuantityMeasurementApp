0627280c21e1a1818a3941fe7976dc8e866448fc
novapathak <novapathak@gmail.com>
2026-02-23 17:25:47 +0530

    [Nova]: modified one file

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -7,7 +7,18 @@ import java.util.*;
  */
 public class QuantityMeasurementApp 
 {
-		
+	public static boolean compareFeet(double value1, double value2) {
+		Feet f1 = new Feet(value1);
+		Feet f2 = new Feet(value2);
+		return f1.equals(f2);
+	}
+	public static boolean compareInches(double value1, double value2) {
+		Feet i1 = new Feet(value1);
+		Feet i2 = new Feet(value2);
+		return i1.equals(i2);
+	}
+	
+	
     public static void main( String[] args )
     {
     	Scanner sc = new Scanner(System.in);
@@ -17,12 +28,15 @@ public class QuantityMeasurementApp
     	System.out.println("Enter second value in feet: ");
     	double feet2 = Double.parseDouble(sc.nextLine());
     	
+    	System.out.println("Equal ? ( " + compareInches(feet1,feet2)+" )");
     	
-    	Feet f1 = new Feet(feet1);
-    	Feet f2 = new Feet(feet2);
+    	System.out.println("Enter first value in Inches: ");
+    	double Inch1 = Double.parseDouble(sc.nextLine());
+    	System.out.println("Enter second value in Inches: ");
+    	double Inch2 = Double.parseDouble(sc.nextLine());
     	
-    	boolean result = f1.equals(f2);
-    	System.out.println("Are the two values equal?   "+ result);
+    	System.out.println("Equal ? ( " + compareInches(Inch1, Inch2)+ " )");
+    	
     	}
     	catch (NumberFormatException e) {
     		System.out.println("Invalid input! Please enter numeric values only");
