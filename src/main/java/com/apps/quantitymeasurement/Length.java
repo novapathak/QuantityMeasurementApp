98b4bc005c7f2a4e49bfa209da4847ec0ecd5e22
novapathak <novapathak@gmail.com>
2026-02-24 01:32:09 +0530

    [Nova]: added file in UC03

---
+++ src/main/java/com/apps/quantitymeasurement/Length.java
@@ -0,0 +1,44 @@
+package com.apps.quantitymeasurement;
+
+public class Length {
+	
+	private double value;
+	private LengthUnit unit;
+	
+	public Length(double value, LengthUnit unit) {
+		this.value = value;
+		this.unit = unit;
+	}
+	
+	private double convertToBaseUnit(){
+		
+		return value*unit.getConversionFactorToFeet();	
+	}
+	
+	public boolean compare(Length other) {
+		if(other==null) {
+			return false;
+		}
+		return Double.compare(this.convertToBaseUnit(), other.convertToBaseUnit())==0;
+		
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this==obj) {
+			return true;
+		}
+		if(obj==null || getClass() != obj.getClass()) {
+			return false;
+		}
+		Length other = (Length)obj;
+		return this.compare(other);
+	}
+	
+	@Override
+	public String toString() {
+		return "Quantity(" +value +", "+unit + ")";
+	}
+
+
+}
