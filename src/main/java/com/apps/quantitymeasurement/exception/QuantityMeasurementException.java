cfa33ad6b23275d1b31af607defa895ea284f1da
novapathak <novapathak@gmail.com>
2026-03-16 11:49:27 +0530

    [Nova] : added QuantityMeasurementException.java in exception

---
+++ src/main/java/com/apps/quantitymeasurement/exception/QuantityMeasurementException.java
@@ -0,0 +1,10 @@
+package com.apps.quantitymeasurement.exception;
+
+public class QuantityMeasurementException extends RuntimeException{
+	
+	private final long serialVersionUID = 1L;
+	public QuantityMeasurementException(String message) {
+		super(message);	
+	}
+
+}
