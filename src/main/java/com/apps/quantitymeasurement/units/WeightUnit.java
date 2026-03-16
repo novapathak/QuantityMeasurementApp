19388d599aa371ab87e437724c1ae94ec951f5f1
novapathak <novapathak@gmail.com>
2026-03-16 11:49:38 +0530

    [Nova] : added WeightUnit.java in units

---
+++ src/main/java/com/apps/quantitymeasurement/units/WeightUnit.java
@@ -0,0 +1,50 @@
+package com.apps.quantitymeasurement.units;
+
+import com.apps.quantitymeasurement.core.IMeasurable;
+
+public enum WeightUnit implements IMeasurable {
+
+	GRAM(1.0), KILOGRAM(1000.0), POUND(453.592);
+
+	// relative to gram conversion factor
+	private final double conversionFactor;
+
+	// constructor
+	WeightUnit(double conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
+
+	@Override
+	public double getConversionFactor() {
+		return conversionFactor;
+	}
+
+	@Override
+	public double convertToBaseUnit(double value) {
+		return value * conversionFactor;
+	}
+
+	@Override
+	public double convertFromBaseUnit(double baseValue) {
+		return baseValue / conversionFactor;
+	}
+
+	@Override
+	public String getUnitName() {
+		return this.name();
+	}
+	
+	
+	public String getMeasurementType() {
+		return this.getClass().getSimpleName();
+	}
+	
+	public IMeasurable getUnitInstance(String unitName) {
+		for(WeightUnit unit : WeightUnit.values()) {
+			if(unit.getUnitName().equalsIgnoreCase(unitName)) {
+				return unit;
+			}
+		}
+		throw new IllegalArgumentException("Invalid length unit: "+ unitName);
+	}
+}
