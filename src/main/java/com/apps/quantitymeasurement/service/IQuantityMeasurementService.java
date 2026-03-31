88e28540cd58111583a13723528022bff93645cb
novapathak <novapathak@gmail.com>
2026-04-01 01:19:50 +0530

    [Nova] : updated IQuantityMeasurementService.java in service

--- src/main/java/com/apps/quantitymeasurement/service/IQuantityMeasurementService.java
+++ src/main/java/com/apps/quantitymeasurement/service/IQuantityMeasurementService.java
@@ -1,13 +1,23 @@
 package com.apps.quantitymeasurement.service;
 
 import com.apps.quantitymeasurement.model.QuantityDTO;
+import com.apps.quantitymeasurement.model.QuantityMeasurementDTO;
+
+import java.util.List;
 
 public interface IQuantityMeasurementService {
 
-   public boolean compare(QuantityDTO q1, QuantityDTO q2);
-   public QuantityDTO convert(QuantityDTO quantity, QuantityDTO.IMeasurableUnit targetUnit);
-   public  QuantityDTO add(QuantityDTO q1, QuantityDTO q2);
-   public  QuantityDTO subtract(QuantityDTO q1, QuantityDTO q2);
-   public double divide(QuantityDTO q1, QuantityDTO q2);
+    QuantityMeasurementDTO add(QuantityDTO q1, QuantityDTO q2);
 
+    QuantityMeasurementDTO subtract(QuantityDTO q1, QuantityDTO q2);
+
+    QuantityMeasurementDTO divide(QuantityDTO q1, QuantityDTO q2);
+
+    QuantityMeasurementDTO compare(QuantityDTO q1, QuantityDTO q2);
+
+    QuantityMeasurementDTO convert(QuantityDTO q1, QuantityDTO q2);
+
+    List<QuantityMeasurementDTO> getHistoryByOperation(String operation);
+
+    long countByOperation(String operation);
 }
