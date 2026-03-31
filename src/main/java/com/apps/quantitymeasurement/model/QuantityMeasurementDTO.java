84358bcb76e08f4503bce648cb473de9bf0fc9dd
novapathak <novapathak@gmail.com>
2026-04-01 01:19:44 +0530

    [Nova] : added QuantityMeasurementDTO.java in model

---
+++ src/main/java/com/apps/quantitymeasurement/model/QuantityMeasurementDTO.java
@@ -0,0 +1,46 @@
+package com.apps.quantitymeasurement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class QuantityMeasurementDTO {
+
+    private Double thisValue;
+    private String thisUnit;
+    private String thisMeasurementType;
+
+    private Double thatValue;
+    private String thatUnit;
+    private String thatMeasurementType;
+
+    private String operation;
+
+    private Double resultValue;
+    private String resultUnit;
+    private String resultMeasurementType;
+
+    private String resultString;
+
+    private String errorMessage;
+    private boolean error;
+
+    public static QuantityMeasurementDTO fromEntity(QuantityMeasurementEntity e) {
+        return new QuantityMeasurementDTO(
+                e.getThisValue(),
+                e.getThisUnit(),
+                e.getThisMeasurementType(),
+                e.getThatValue(),
+                e.getThatUnit(),
+                e.getThatMeasurementType(),
+                e.getOperation(),
+                e.getResultValue(),
+                e.getResultUnit(),
+                e.getResultMeasurementType(),
+                e.getResultString(),
+                e.getErrorMessage(),
+                e.isError()
+        );
+    }
+}
