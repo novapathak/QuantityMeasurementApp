2cac34fbe4d66079330a5282f9dfdf6731a04537
novapathak <novapathak@gmail.com>
2026-04-01 12:30:32 +0530

    [Nova] : updated QuantityInputDTO.java in model

--- src/main/java/com/apps/quantitymeasurement/model/QuantityInputDTO.java
+++ src/main/java/com/apps/quantitymeasurement/model/QuantityInputDTO.java
@@ -1,9 +1,20 @@
 package com.apps.quantitymeasurement.model;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QuantityInputDTO {
+
+    @Valid
+    @NotNull(message = "thisQuantityDTO is required")
     private QuantityDTO thisQuantityDTO;
+
+    @Valid
     private QuantityDTO thatQuantityDTO;
 }
