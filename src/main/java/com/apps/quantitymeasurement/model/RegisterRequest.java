2b8b3eb2419f71c689448f5ee2b4f3b00fdaa15f
novapathak <novapathak@gmail.com>
2026-04-01 15:44:08 +0530

    [Nova] : added RegisterRequest.java in model

---
+++ src/main/java/com/apps/quantitymeasurement/model/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.apps.quantitymeasurement.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 100, message = "Username must be between 3 and 100 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Full name is required")
+    @Size(max = 150, message = "Full name must not exceed 150 characters")
+    private String fullName;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
+    private String password;
+}
